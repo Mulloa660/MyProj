5e687be715c8943ac95202ca7bd3a402417942ac
Michelle Ulloa <umichelle244@gmail.com>
2023-04-12 11:43:59 -0400

    add a new MyProj class

---
+++ src/main/java/com/learntocode/ShoppingList/Java.java
@@ -0,0 +1,9 @@
+package com.learntocode.ShoppingList;
+
+import com.sun.tools.javac.Main;
+
+public class Java {
+    public static void main(String[] args){
+        System.out.println ("Apples");
+    }
+}
